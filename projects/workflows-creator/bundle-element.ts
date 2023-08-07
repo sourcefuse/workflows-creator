b0b3284ea2796d990f3ddf0a57debb34cdd6b93e
yeshamavani <83634146+yeshamavani@users.noreply.github.com>
2023-08-07 17:58:55 +0530

    feat(core): changes to publish the library as web component (#17)
    * chore(ci-cd): feat(core): changes to publish the library as web component
    
    GH-16
    * chore(ci-cd): chore(ci-cd): add release workflow for library and element
    
    GH-16
    * chore(ci-cd): chore(ci-cd): changes to publish the element as well
    
    GH-16

--- projects/workflows-creator/bundle-element.ts
+++ projects/workflows-creator/bundle-element.ts
@@ -2,38 +2,36 @@ import * as crypto from 'crypto';
 import {ensureDir, readFileSync, writeFileSync} from 'fs-extra';
 const {copyFile, rm} = require('fs-extra');
 const concat = require('concat');
+
 async function elementsBundler() {
-  const space = 4;
+  const space = 2;
   const files = [
-    '../../dist/workflows-element/runtime.js',
-    '../../dist/workflows-element/polyfills.js',
-    '../../dist/workflows-element/main.js',
+    './dist/workflows-element/runtime.js',
+    './dist/workflows-element/polyfills.js',
+    './dist/workflows-element/main.js',
   ];
-  await ensureDir('../../dist/workflows-element/element');
-  await concat(files, '../../dist/workflows-element/element/workflows-element.js');
+  await ensureDir('../workflows-element/dist');
+  await concat(files, '../workflows-element/dist/workflows-element.js');
   await copyFile(
-    '../../dist/workflows-element/styles.css',
-    '../../dist/workflows-element/element/styles.css',
+    './dist/workflows-element/styles.css',
+    '../workflows-element/dist/styles.css',
   );
 
-  await copyFile(
-    '../../dist/workflows-creator/README.md',
-    '../../dist/workflows-element/element/README.md',
-  );
+  await copyFile('./dist/README.md', '../workflows-element/dist/README.md');
 
-  // whenever any changes are made in the element the corresponding js file will
-  // resulting in changing its hash, so the element will also be published
+  // generate the hash of element file to keep a track of the chnages
+  // whenever any changes are made in the library the corresponding js file will
+  // resul in changing its hash, so the element will also be published
 
-  const file = '../../dist/workflows-element/element/workflows-element.js';
+  const file = '../workflows-element/dist/workflows-element.js';
   const data = readFileSync(file);
   const hash = crypto.createHash('sha256');
   hash.update(data);
   const fileHash = hash.digest('hex');
 
- 
   //if there will be changes in hash then the element will also be published
 
-  const packageJsonPath = '../../dist/workflows-creator/package.json';
+  const packageJsonPath = '../workflows-element/package.json';
   const jsonObj = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
   jsonObj.hash = fileHash;
   writeFileSync(packageJsonPath, JSON.stringify(jsonObj, null, space));
