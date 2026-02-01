69c18851bf91ad99a458bb812f4085b3c3e1d1f0
Akanksha13-dev <84703175+Akanksha13-dev@users.noreply.github.com>
2026-02-01 20:44:16 +0530

    feat(core): allow events and action to enable disable based on toggle feature (#102)
    * feat(core): allow events and action to enable disabke based on toggle feature
    
    * feat(core): fix all event change req
    
    ---------
    
    Co-authored-by: Akanksha	 Singh <akanksha.singh@Akanksha-SFIN943.local>

--- projects/workflows-creator/src/lib/classes/nodes/abstract-workflow-action.class.ts
+++ projects/workflows-creator/src/lib/classes/nodes/abstract-workflow-action.class.ts
@@ -4,4 +4,14 @@ import {AbstractWorkflowNode} from './abstract-workflow-node.class';
 export abstract class WorkflowAction<E> extends AbstractWorkflowNode<E> {
   abstract isElseAction: boolean;
   type = NodeTypes.ACTION;
+
+  private static _isEnabled = true;
+
+  isEnabled(): boolean {
+    return (this.constructor as typeof WorkflowAction)._isEnabled;
+  }
+
+  static setEnabled(value: boolean): void {
+    this._isEnabled = value;
+  }
 }
--- projects/workflows-creator/src/lib/classes/nodes/abstract-workflow-event.class.ts
+++ projects/workflows-creator/src/lib/classes/nodes/abstract-workflow-event.class.ts
@@ -5,4 +5,13 @@ export abstract class WorkflowEvent<E> extends AbstractWorkflowNode<E> {
   abstract trigger: boolean;
   type = NodeTypes.EVENT;
   startElement: string;
+  private static _isEnabled = true;
+
+  isEnabled(): boolean {
+    return (this.constructor as typeof WorkflowEvent)._isEnabled;
+  }
+
+  static setEnabled(value: boolean): void {
+    this._isEnabled = value;
+  }
 }
